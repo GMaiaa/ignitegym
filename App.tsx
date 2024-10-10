5ad571ec4143dad5dae3d0161441e1ca1ebaa7e8
GMaiaa <gabrielmaiaa22@gmail.com>
2024-10-09 21:27:08 -0300

    Starts ignitegym

---
+++ App.tsx
@@ -0,0 +1,29 @@
+import { StatusBar } from 'react-native';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
+import { Center, GluestackUIProvider, Text } from '@gluestack-ui/themed';
+
+import { config } from './config/gluestack-ui.config';
+
+import { Routes } from '@routes/index';
+import { Loading } from '@components/Loading';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+
+  return (
+    <GluestackUIProvider config={config}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? (
+        <Routes />
+      ) : (
+        <Loading />
+      )}
+    </GluestackUIProvider>
+
+  );
+}
+
